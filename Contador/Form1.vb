ea4e5ee1860b1209563e8ad8b315750bedc32b73
Paulo Côto <Paulo Cto@WIN-GESFLHLR4EE>
2019-10-24 11:34:59 +0100

    Adicionar arquivos de projeto.

---
+++ Contador/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Dim contador = 0
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        contador += 1
+        Dim tempo = TimeSpan.FromSeconds(contador)
+        Label1.Text = tempo.ToString
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        contador = 0
+        Label1.Text = "00:00:00"
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 1000
+    End Sub
+End Class
